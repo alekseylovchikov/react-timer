e87558b1abae224e092e3b4d64d8f6086a2ac57a
Aleksey Lovchikov <svdmusic@gmail.com>
2016-09-27 21:52:34 +0300

    countdown component - create timer

--- src/components/Countdown.jsx
+++ src/components/Countdown.jsx
@@ -6,12 +6,31 @@ var CountdownForm = require('CountdownForm');
 var Countdown = React.createClass({
   getInitialState: function() {
     return {
-      count: 0
+      count: 0,
+      countdownStatus: 'stopped'
     };
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
+  },
+  startTimer: function() {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({
+        count: newCount >= 0 ? newCount : 0
+      });
+    }, 1000);
   },
   handleSetCountdown: function(sec) {
     this.setState({
-      count: sec
+      count: sec,
+      countdownStatus: 'started'
     });
   },
   render: function() {
